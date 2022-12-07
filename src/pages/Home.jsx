e9058ee0456287a4607008921b70529f311d1096
IndudiniThennakoon <thennakoonindudini@gmail.com>
2022-12-07 19:25:14 +0530

    styles(home) : bootstrap grid system

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Home.css';
+
 // import Card from "../Card/Card";
 
 function Home() {
@@ -33,13 +34,33 @@ function Home() {
         //     </div>
         //     <button className="btn btn-outline-primary">Submit</button>
         // </div>
+        <div>
+            <div className="row">
+                <div className="col-6 bg-danger">Column 01</div>
+                <div className="col-6 bg-primary">Column 02</div>
+            </div>
+            <div className="row">
+                <div className="col-4 bg-warning">Column 03</div>
+                <div className="col-4 bg-success">Column 06</div>
+                <div className="col-4 bg-warning">Column 07</div>
+            </div>
 
-        <div className="row bg-dark">
-            <div className="col-12 bg-danger">Column 01</div>
-            <div className="col-6 bg-primary">Column 02</div>
-            <div className="col-4 bg-warning">Column 03</div>
+            <div className="row">
+                <div className="col-4 bg-danger">Column 04</div>
+                <div className="col-4 bg-primary">Column 08</div>
+                <div className="col-4 bg-dark text-white">Column 09</div>
+            </div>
+
+            <div className="row">
+                <div className="col-4 bg-success">Column 05</div>
+                <div className="col-4 bg-warning">Column 10</div>
+                <div className="col-4 bg-primary">Column 11</div>
+            </div>
+
         </div>
-    );
+
+    )
+        ;
 }
 
 export default Home;
