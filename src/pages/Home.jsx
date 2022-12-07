de59ab91f3aff209b639732ad52d500eda54208e
IndudiniThennakoon <thennakoonindudini@gmail.com>
2022-12-07 18:40:56 +0530

    styles(home) : bootstrap grid system

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,43 @@
 import React from 'react';
 import './Home.css';
-import Card from "../Card/Card";
+// import Card from "../Card/Card";
 
 function Home() {
     return (
-        <div>
-            <div className="cardList">
-                <Card title="Indudini"
-                      para="This is para 1.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam error facilis
-                  molestiae natus nulla quas quos. Adipisci amet animi consequuntur enim eos excepturi facere facilis!"/>
-                <Card title="Madhusha"
-                      para="This is para 2.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam error facilis
-                  molestiae natus nulla quas quos. Adipisci amet animi consequuntur enim eos excepturi facere facilis!"/>
-                <Card title="Indu"
-                      para="This is para 3 .Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam error facilis
-                  molestiae natus nulla quas quos. Adipisci amet animi consequuntur enim eos excepturi facere facilis!"/>
-                <Card title="Madhus"
-                      para="This is para 4.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam error facilis
-                  molestiae natus nulla quas quos. Adipisci amet animi consequuntur enim eos excepturi facere facilis!"/>
-                <Card title="Indudini"
-                      para="This is para 5.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam error facilis
-                  molestiae natus nulla quas quos. Adipisci amet animi consequuntur enim eos excepturi facere facilis!"/>
-                <Card title="Madhusha"
-                      para="This is para 6.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam error facilis
-                  molestiae natus nulla quas quos. Adipisci amet animi consequuntur enim eos excepturi facere facilis!"/>
-                <Card title="Indu"
-                      para="This is para 7 .Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam error facilis
-                  molestiae natus nulla quas quos. Adipisci amet animi consequuntur enim eos excepturi facere facilis!"/>
-                <Card title="Madhus"
-                      para="This is para 8.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam error facilis
-                  molestiae natus nulla quas quos. Adipisci amet animi consequuntur enim eos excepturi facere facilis!"/>
-            </div>
-            <button className="btn btn-outline-primary">Submit</button>
+        // <div>
+        //     <div className="cardList">
+        //         <Card title="Indudini"
+        //               para="This is para 1.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam error facilis
+        //           molestiae natus nulla quas quos. Adipisci amet animi consequuntur enim eos excepturi facere facilis!"/>
+        //         <Card title="Madhusha"
+        //               para="This is para 2.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam error facilis
+        //           molestiae natus nulla quas quos. Adipisci amet animi consequuntur enim eos excepturi facere facilis!"/>
+        //         <Card title="Indu"
+        //               para="This is para 3 .Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam error facilis
+        //           molestiae natus nulla quas quos. Adipisci amet animi consequuntur enim eos excepturi facere facilis!"/>
+        //         <Card title="Madhus"
+        //               para="This is para 4.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam error facilis
+        //           molestiae natus nulla quas quos. Adipisci amet animi consequuntur enim eos excepturi facere facilis!"/>
+        //         <Card title="Indudini"
+        //               para="This is para 5.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam error facilis
+        //           molestiae natus nulla quas quos. Adipisci amet animi consequuntur enim eos excepturi facere facilis!"/>
+        //         <Card title="Madhusha"
+        //               para="This is para 6.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam error facilis
+        //           molestiae natus nulla quas quos. Adipisci amet animi consequuntur enim eos excepturi facere facilis!"/>
+        //         <Card title="Indu"
+        //               para="This is para 7 .Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam error facilis
+        //           molestiae natus nulla quas quos. Adipisci amet animi consequuntur enim eos excepturi facere facilis!"/>
+        //         <Card title="Madhus"
+        //               para="This is para 8.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam error facilis
+        //           molestiae natus nulla quas quos. Adipisci amet animi consequuntur enim eos excepturi facere facilis!"/>
+        //     </div>
+        //     <button className="btn btn-outline-primary">Submit</button>
+        // </div>
+
+        <div className="row bg-dark">
+            <div className="col-12 bg-danger">Column 01</div>
+            <div className="col-6 bg-primary">Column 02</div>
+            <div className="col-4 bg-warning">Column 03</div>
         </div>
     );
 }
