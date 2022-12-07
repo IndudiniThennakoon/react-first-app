e22820e23ccee0a3c9d569d818c65fced4ee5764
IndudiniThennakoon <thennakoonindudini@gmail.com>
2022-12-07 16:40:07 +0530

    feat(project) : create card in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import './Home.css';
 
 function Home() {
     return (
         <div>
-            Home
+            <div className='card'>
+                <h1 className='cardTitle'>Title</h1>
+                <p className='para'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab amet, asperiores beatae laudantium
+                    libero magnam nobis, nulla officiis quibusdam rem sapiente ullam vel veniam voluptate, voluptates!
+                    Enim, modi, nulla! Tempora?</p>
+            </div>
+
+            <div className='card'>
+                <h1 className='cardTitle'>Title</h1>
+                <p className='para'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab amet, asperiores beatae laudantium
+                    libero magnam nobis, nulla officiis quibusdam rem sapiente ullam vel veniam voluptate, voluptates!
+                    Enim, modi, nulla! Tempora?</p>
+            </div>
+
+            <div className='card'>
+                <h1 className='cardTitle'>Title</h1>
+                <p className='para'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab amet, asperiores beatae laudantium
+                    libero magnam nobis, nulla officiis quibusdam rem sapiente ullam vel veniam voluptate, voluptates!
+                    Enim, modi, nulla! Tempora?</p>
+            </div>
+
+            <div className='card'>
+                <h1 className='cardTitle'>Title</h1>
+                <p className='para'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab amet, asperiores beatae laudantium
+                    libero magnam nobis, nulla officiis quibusdam rem sapiente ullam vel veniam voluptate, voluptates!
+                    Enim, modi, nulla! Tempora?</p>
+            </div>
+
         </div>
     );
 
